df31ceff67ef47387672843b9749be54ee3cb4e5
zahra <zahrashahzehi@gmail.com>
2021-07-18 20:07:22 +0430

    test

---
+++ map.py
@@ -0,0 +1,14 @@
+numbers = [1,2,3,4,5]
+doublse = list(map(lambda num : num *2 , numbers))
+print(doublse)
+print(doublse)
+print("==========================")
+names = ["zahra" , "atta" , "sara"]
+upper_name = list(map(lambda name : name.upper() , names))
+print(upper_name)
+print("======================================")
+pepale = [{"name" : "zahra" , "family" : "shahzehi" , "age" : 20} ,
+{"name":"atta" , "family": "attarian" , "age" : 21}
+, {"name" : "amir" , "family" : "ajam" , "age" : 23}]
+family = list(map(lambda person : person["family"] , pepale))
+print(family)
